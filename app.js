305381c9a6cad272cc4a1b5f1bd1d0db65e54a3e
madhavrajverma <madhavrajverma175@gmail.com>
2024-02-12 20:22:18 +0530

    new

--- app.js
+++ app.js
@@ -42,10 +42,11 @@ app.use(session({secret: 'my secret',resave : false,saveUninitialized:false,stor
 
 app.use(shopRoutes);
 
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(err => {
     console.log(err);
